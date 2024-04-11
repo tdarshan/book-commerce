ec728ec13aab0c800f1367b724d5e77f7d593d5c
Darshan Trivedi <trivedidarshan14@gmail.com>
2024-04-11 14:39:41 +0530

    Fix: Updated API endpoint for live app in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       '/api': {
         target: 'https://d1krvzwx5oquy1.cloudfront.net',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, 'https://d1krvzwx5oquy1.cloudfront.net'),
       }
     }
   },
