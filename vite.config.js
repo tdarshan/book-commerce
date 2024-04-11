0bf84922e09fe60f990a6e8043451cabd7586832
Darshan Trivedi <trivedidarshan14@gmail.com>
2024-04-11 14:50:20 +0530

    Fix: Updated API endpoint for live app

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
+      'api': {
         target: 'https://d1krvzwx5oquy1.cloudfront.net',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, 'https://d1krvzwx5oquy1.cloudfront.net'),
