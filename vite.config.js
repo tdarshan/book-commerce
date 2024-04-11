fc20a4d58eda799087d296b7ce9239743a5b395a
Darshan Trivedi <trivedidarshan14@gmail.com>
2024-04-11 14:26:14 +0530

    Fix: added proxy and configuratin for API call for solving cors error

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://d1krvzwx5oquy1.cloudfront.net',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  },
   plugins: [react()],
 })
